56e4cfd4ea1bb88812b2f1122abbcb8a3353e163
Το μοχθηρό ^_^ <69906215+Malus-risus@users.noreply.github.com>
2024-04-05 16:48:34 +0800

    Update QSTileService.kt

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/service/QSTileService.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/service/QSTileService.kt
@@ -14,39 +14,50 @@ import com.v2ray.ang.R
 import com.v2ray.ang.util.MessageUtil
 import com.v2ray.ang.util.Utils
 import java.lang.ref.SoftReference
+import java.lang.ref.WeakReference
 
 @TargetApi(Build.VERSION_CODES.N)
 class QSTileService : TileService() {
 
-    private val appName by lazy { getString(R.string.app_name) }
-    private val inactiveIcon by lazy { Icon.createWithResource(applicationContext, R.drawable.ic_stat_name) }
+    private lateinit var appName: String
+    private lateinit var activeIcon: Icon
+    private lateinit var inactiveIcon: Icon
+    private var mMsgReceive: BroadcastReceiver? = null
 
-    private fun setState(state: Int) {
-        val tile = qsTile
-        val label = if (state == Tile.STATE_ACTIVE) V2RayServiceManager.currentConfig?.remarks else appName
-        tile?.apply {
-            this.state = state
-            this.label = label
-            this.icon = inactiveIcon
-            updateTile()
-        }
+    override fun onCreate() {
+        super.onCreate()
+        appName = getString(R.string.app_name)
+        activeIcon = Icon.createWithResource(applicationContext, R.drawable.ic_stat_name)
+        inactiveIcon = Icon.createWithResource(applicationContext, R.drawable.ic_stat_name)
+    }
+
+    fun setState(state: Int) {
+        val label = if (state == Tile.STATE_ACTIVE) V2RayServiceManager.currentConfig?.remarks ?: appName else appName
+        val icon = if (state == Tile.STATE_ACTIVE) activeIcon else inactiveIcon
+
+        qsTile?.state = state
+        qsTile?.label = label
+        qsTile?.icon = icon
+
+        qsTile?.updateTile()
     }
 
     override fun onStartListening() {
         super.onStartListening()
         setState(Tile.STATE_INACTIVE)
-        mMsgReceive = ReceiveMessageHandler()
-        val intentFilter = IntentFilter(AppConfig.BROADCAST_ACTION_ACTIVITY).apply {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                addFlags(Context.RECEIVER_EXPORTED)
-            }
+        mMsgReceive = ReceiveMessageHandler(this)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            registerReceiver(mMsgReceive, IntentFilter(AppConfig.BROADCAST_ACTION_ACTIVITY), Context.RECEIVER_EXPORTED)
+        } else {
+            registerReceiver(mMsgReceive, IntentFilter(AppConfig.BROADCAST_ACTION_ACTIVITY))
         }
-        registerReceiver(mMsgReceive, intentFilter)
+
         MessageUtil.sendMsg2Service(this, AppConfig.MSG_REGISTER_CLIENT, "")
     }
 
     override fun onStopListening() {
         super.onStopListening()
+
         unregisterReceiver(mMsgReceive)
         mMsgReceive = null
     }
@@ -59,16 +70,18 @@ class QSTileService : TileService() {
         }
     }
 
-    private var mMsgReceive: BroadcastReceiver? = null
+    private class ReceiveMessageHandler(service: QSTileService) : BroadcastReceiver() {
+        private val serviceRef: WeakReference<QSTileService> = WeakReference(service)
 
-    private inner class ReceiveMessageHandler : BroadcastReceiver() {
         override fun onReceive(ctx: Context?, intent: Intent?) {
-            when (intent?.getIntExtra("key", 0)) {
-                AppConfig.MSG_STATE_RUNNING, 
-                AppConfig.MSG_STATE_START_SUCCESS -> setState(Tile.STATE_ACTIVE)
-                AppConfig.MSG_STATE_NOT_RUNNING, 
-                AppConfig.MSG_STATE_START_FAILURE, 
-                AppConfig.MSG_STATE_STOP_SUCCESS -> setState(Tile.STATE_INACTIVE)
+            serviceRef.get()?.let { service ->
+                when (intent?.getIntExtra("key", 0)) {
+                    AppConfig.MSG_STATE_RUNNING -> service.setState(Tile.STATE_ACTIVE)
+                    AppConfig.MSG_STATE_NOT_RUNNING -> service.setState(Tile.STATE_INACTIVE)
+                    AppConfig.MSG_STATE_START_SUCCESS -> service.setState(Tile.STATE_ACTIVE)
+                    AppConfig.MSG_STATE_START_FAILURE -> service.setState(Tile.STATE_INACTIVE)
+                    AppConfig.MSG_STATE_STOP_SUCCESS -> service.setState(Tile.STATE_INACTIVE)
+                }
             }
         }
     }
